c8570a6c59129c57fccc58df53018b1fcebc525c
Babayiğit <babayigit@gmail.com>
2024-08-12 11:40:29 +0300

    babayigit v2

--- src/Murat.java
+++ src/Murat.java
@@ -2,6 +2,7 @@ public class Murat {
     public static void main(String[] args) {
 
         System.out.println("ne yaparsan yap Aşk ile yap!");
+        System.out.println("bu collabration bizi biraz gerdi\nGerginlik iyidir insanı zinde tutar");
 
     }
 
