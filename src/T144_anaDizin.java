5c54c661c76ff316589b802490b29a924abdea2e
dilek sengul <av.dilekylcnn@gmail.com>
2024-08-12 12:23:24 +0300

    Dilek v2

--- src/T144_anaDizin.java
+++ src/T144_anaDizin.java
@@ -1,6 +1,6 @@
 public class T144_anaDizin {
     public static void main(String[] args) {
         System.out.println("Buradan Murat Geçti!");
+        System.out.println("Buralar karisacak gibi");
     }
-
 }
