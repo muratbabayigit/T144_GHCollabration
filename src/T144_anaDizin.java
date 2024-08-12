9a0923baa6869dbff39b803cdb1c9968b0f0be8d
Babayiğit <babayigit@gmail.com>
2024-08-12 12:30:05 +0300

    babayigit v4

--- src/T144_anaDizin.java
+++ src/T144_anaDizin.java
@@ -2,6 +2,7 @@ public class T144_anaDizin {
     public static void main(String[] args) {
         System.out.println("Buradan Murat Geçti!");
         System.out.println("Buralar karışacak alın vaziyet alın");
+
     }
 
 }
