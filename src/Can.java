a9f76273b670dbe6d0ac9a9dacc0dd30a45cd143
Can Yilmaz <canyilmaz9999@gmail.com>
2024-08-12 10:31:56 +0200

    Can v1

---
+++ src/Can.java
@@ -0,0 +1,5 @@
+public class Can {
+    public static void main(String[] args) {
+        System.out.println("Bu sinif Can tarafindan olusturulmustur");
+    }
+}
--- src/Murat.java
+++ src/Murat.java
@@ -1,5 +1,8 @@
 public class Murat {
     public static void main(String[] args) {
+
         System.out.println("ne yaparsan yap Aşk ile yap!");
+
     }
+
 }
